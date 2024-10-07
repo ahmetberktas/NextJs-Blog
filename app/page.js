428f8872ea8e96aa76d7fc395a3a353a9e00f7dc
ahmetberktas <ahmetberktas50@gmail.com>
2024-10-07 21:13:50 +0300

    Blog Card componnt

--- app/page.js
+++ app/page.js
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <div>Home</div>;
+import BlogCard from "@/components/BlogCard";
+
+async function getPosts() {
+  const response = await fetch("https://dummyjson.com/posts?limit=10");
+  return response.json();
+}
+
+export default async function Home() {
+  const { posts } = await getPosts();
+  return (
+    <div>
+      {posts.map((post) => (
+        <BlogCard key={post.id} {...post} />
+      ))}
+    </div>
+  );
 }
