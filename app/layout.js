1a9fcd516915f1331d26abea4d81c9afc121cd4c
ahmetberktas <ahmetberktas50@gmail.com>
2024-10-07 19:59:01 +0300

    Update layout and styles

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,26 @@
+import { Inter } from "next/font/google";
+import styles from "./styles.module.css";
+import "./global.css";
+import Head from "./head";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="tr" className={inter.className}>
+      <Head></Head>
+      <body className={styles.container}>
+        <header>
+          <p>Header</p>
+        </header>
+        <main>{children}</main>
+        <footer>
+          <p>Footer</p>
+        </footer>
+      </body>
     </html>
   );
 }
